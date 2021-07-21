cf7e09f366a6d43bc338499ec1ae75059c767f28
Arthur <dev.arthur.vardanyan@gmail.com>
2021-07-21 15:33:38 +0400

    Add TabLayout

--- app/src/main/java/com/insta/viewpager/MainActivity.kt
+++ app/src/main/java/com/insta/viewpager/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.insta.viewpager.databinding.ActivityMainBinding
 
 class MainActivity : FragmentActivity() {
@@ -18,5 +20,17 @@ class MainActivity : FragmentActivity() {
 
         adapter = NumberAdapter(this)
         binding?.viewPager?.adapter = adapter
+
+
+        TabLayoutMediator(binding?.tabLayout!!, binding?.viewPager!!) { tab, position ->
+            tab.text = "TAB ${position + 1}"
+
+            if (position == 2) {
+                val badge = tab.orCreateBadge
+                badge.number = 1
+            }
+
+        }.attach()
+
     }
 }
